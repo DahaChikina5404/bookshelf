3ba7e06e99a4b6e21e6f253d5f40cf7e3b3eabbb
DahaChikina5404 <valentina_kpss@mail.ru>
2023-07-18 20:11:21 +0700

    Добавлено удаление и добавление объекта массива

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const books = [
     {
+        id: 1,
         title: 'Мастер и Маргарита',
         authors: 'Михаил Булгаков',
         year: '1940',
@@ -7,6 +8,7 @@ const books = [
     },
 
     {
+        id: 2,
         title: 'Чайка по имени Джонатан Ливингстон',
         authors: 'Ричард Бах',
         year: '1970',
@@ -14,6 +16,7 @@ const books = [
     },
 
     {
+        id: 3,
         title: 'Хождение по мукам',
         authors: 'Алексей Толстой',
         year: '1941',
@@ -21,8 +24,9 @@ const books = [
     },
 
     {
+        id: 4,
         title: 'Три чашки чая',
-        authors: 'Мортенсон Грег, Релин Оливер Дэвид',
+        authors: 'Грег Мортенсон, Дэвид Релин Оливер',
         year: '2006',
         image: 'src="https://img3.labirint.ru/rc/bd7a48187d7bb49ab991a15854be3d12/363x561q80/books28/272367/cover.jpg?1612520752"'
     }
@@ -41,8 +45,77 @@ function renderBooks() {
                 <p>${book.authors}</p>
                 <p>${book.year}</p>
                 <button class="button_style">Изменить</button>
-                <button class="button_style">Удалить</button>
+                <button onclick='deleteBooks(${book.id})' class="button_style">Удалить</button>
             </div>`
     })
 }   
+
+// Удалить книгу
+
+function deleteBooks(id) {
+        const book = books.find((number) => {
+            return number.id === id
+        })
+
+        const bookIndex = books.indexOf(book)
+        books.splice(bookIndex, 1)
+        renderBooks()
+}
+
+// Очистка поля ввода
+
+function cleanForm() {
+        document.getElementById('image').value = ""
+        document.getElementById('title').value = ""
+        document.getElementById('authors').value = ""
+        document.getElementById('year').value = ""
+}
+
+
+
+// ОТКРЫВАЕМ ПОЛЯ ДЛЯ ВВОДА КНИГИ
+// объявляем переменную на открытие полей ввода
+
+let isOpen = false
+
+function openInputBox() {
+
+        const containerAdd = document.getElementById ('container_add-book')
+
+        // Открытие мобильного меню по нажатию кнопки
+
+        if (isOpen) {
+            containerAdd.style.display = 'none'
+            isOpen = false
+        }
+        else {
+            containerAdd.style.display = 'flex'
+            isOpen = true
+        }
+}
+
+
+// Добавить книгу
+
+function addBook() {
+
+        const imageValue = document.getElementById('image').value
+        const titleValue = document.getElementById('title').value
+        const authorsValue = document.getElementById('authors').value
+        const yearValue = document.getElementById('year').value
+
+        const book = {
+            image: imageValue,
+            title: titleValue,
+            authors: authorsValue,
+            year: yearValue
+        }
+
+        books.push(book)
+        renderBooks()
+        cleanForm()
+
+
+}
+
 renderBooks()
